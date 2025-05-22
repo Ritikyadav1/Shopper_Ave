d73199eb6da9dadbbb2fdba5f22674b90197e97c
Ritik Yadav <ritikyadav10032004@gmail.com>
2025-05-22 16:51:40 +0530

    First Commit

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,10 @@
+
+function HeroSection() {
+  return (
+    <div>
+        <img src="https://static.vecteezy.com/system/resources/previews/000/669/988/original/vector-shopping-online-banner.jpg" alt="" />
+    </div>
+  )
+}
+
+export default HeroSection
